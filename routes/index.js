18d3dbb33f80e8013459addeabae1b1c4a47e9ff
Jurgis <jurgis.grigorjevs@gmail.com>
2020-04-16 23:40:05 +0700

    picture upload feature added

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,69 @@
 var express = require('express');
 var router = express.Router();
+const upload = require('../utils/upload')
+const { loadData, saveData } = require("../utils/data")
+const fs = require("fs");
+const jimp = require('jimp')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Memezar!' });
 });
+
+router.get("/browse", (req, res) => {
+  const data = loadData()
+  res.render("allImages", { images: data })
+})
+router.put("/memes", (req, res) => {
+  const data = loadData()
+  res.render("allImages", { images: data })
+})
+
+
+function checkDub(req,res,next){
+  const { file } = req//same as req.file, shorter syntax
+  //file upload
+  if (!file) {
+    res.render("index", { error: "you need to upload a file" })
+  }
+  const data = loadData()
+  
+  //use findIndex to compare file names and sizes
+  const found = data.findIndex(el => el.originalname === file.originalname && el.size === file.size) // expect to receive -1 or an index number if found
+  //if there is no duplicate finIndex returns -1
+  if (found !== -1) {
+    //remove from json
+    fs.unlinkSync(file.path) //
+    return res.render("index", { error: "file duplicate error: this file is already uploaded" })
+  }
+  req.data = data
+  next()
+}
+
+
+
 //endpoint for the upload form in index.hbs
-router.post("/upload", (req, res, next)=>{
-  res.render("allImages")
+router.post("/upload", upload, checkDub, async (req, res) => {
+  const {file} = req;
+  let data = req.data
+  try {
+  let image = await jimp.read(file.path)
+  image.resize(jimp.AUTO, 250, jimp.RESIZE_NEAREST_NEIGHBOR);
+  await image.writeAsync(file.path)//will return promise - wait until file ready
+  //add an id to each item before it is pushed
+  file.id = data.length === 0 ? 1 : data[data.length - 1].id + 1
+  data.push(file)
+  saveData(data)
+  
+// why is the color of bg.jpg different
+  return res.render("allImages", { images: data })
+
+  } catch (e) {
+    fs.unlinkSync(file.path)//remove file
+    //e is error object(built in)
+    res.render("index", { error: e.message })
+  }
+  // req.duplicate = true  
+
 })
 module.exports = router;
---
+++ utils/upload.js
@@ -0,0 +1,30 @@
+const multer = require('multer')
+const path = require('path')//current full path
+
+//specify where the files will be saved
+const pathToUpload = path.join(__dirname, "../public/uploads")
+
+//function structure comes from multer template
+const storage = multer.diskStorage({
+    //where to save the files
+    destination: function (req, file, cb) {
+        //first argument is error
+        cb(null, pathToUpload) // if there is no 1st arg, multer will use 2nd as path to folder
+    },
+    //Change the file when it is uploaded
+    filename: function (req, file, cb) {
+        const allows = ["image/jpeg", "image/png", "image/jpg", "image/gif"]
+        //check if the file type uploaded is in format specified above
+        if (!allows.includes(file.mimetype)) {
+            const error = new Error("Ooops. Filetype wrong")
+            return cb (error, undefined)
+        }
+        cb(null, Date.now().toString() + file.originalname) // gotta use this
+        // cb(null,file.originalname)  // or just simply this and remove the unlink :D. i
+    }
+
+})
+//single specifies that only one file can be uploaded. Name must be the same with input field name
+const upload = multer({ storage: storage }).single('fileUpload')
+
+module.exports = upload
